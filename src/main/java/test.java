15034e200a17eb06dbdb85a4080e3b47e1580632
areebfz1 <office.areebfayyaz@gmail.com>
2025-07-18 20:36:13 -0400

    firstcommit

---
+++ src/main/java/test.java
@@ -0,0 +1,8 @@
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("This is to test Git repository");
+
+    }
+}
