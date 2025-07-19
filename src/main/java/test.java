d265adf42de06e262ed092a3fcc929a116ed9d4f
areebfz1 <office.areebfayyaz@gmail.com>
2025-07-18 21:06:52 -0400

    Adding a commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -3,6 +3,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("This is to test Git repository");
+        System.out.println("Adding a commit");
 
     }
 }
